d4e6517569482966e50089ab2d26d71c9444eb49
SterDream <130822110+SterDream@users.noreply.github.com>
2026-09-05 22:49:17 +0900

    Update mel_processing.py

--- mel_processing.py
+++ mel_processing.py
@@ -63,7 +63,7 @@ def spectrogram_torch(y, n_fft, sampling_rate, hop_size, win_size, center=False)
         pad_mode='reflect',
         normalized=False,
         onesided=True,
-        return_complex=False
+        # return_complex=False
     ).to(torch.bfloat16)
     
     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
@@ -128,7 +128,7 @@ def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size,
         pad_mode='reflect',
         normalized=False,
         onesided=True,
-        return_complex=False
+        # return_complex=False
     ).to(torch.bfloat16)
 
     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
